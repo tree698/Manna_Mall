75ed8a08776a0a094430349eb457c49f7822884b
Chanwoo <tree698@gmail.com>
2023-01-29 14:31:21 +0900

    update firebase DB URL

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -12,7 +12,7 @@ import { v4 as uuid } from 'uuid';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
